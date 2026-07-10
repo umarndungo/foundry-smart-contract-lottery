37f4c848caa4baa27c9879ee222dc4b249705f21
umarndungo <omarnjagi@gmail.com>
2026-07-10 11:25:45 +0300

    Raffle: Events and emit events.

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -39,6 +39,10 @@ contract Raffle {
     error Raffle__SendMoreToEnterRaffle();
 
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /** Events */
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee){
         i_entranceFee = entranceFee;
@@ -50,6 +54,9 @@ contract Raffle {
         if (msg.value < i_entranceFee){
             revert Raffle__SendMoreToEnterRaffle();
         }
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEntered(msg.sender);
     }
 
     function pickWinner() public {}
